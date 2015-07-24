fc7261f3adabc0e45733fc6c7a8d17cac47c07bf
Shannon Forsman <sforsman1@gmail.com>
2015-07-24 11:10:56 -0600

    user can view map markers of fire

---
+++ app.js
@@ -0,0 +1,81 @@
+require('dotenv').load()
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var unirest = require('unirest')
+var parseString = require('xml2js').parseString;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.get('/locations', function(req, res){
+  unirest.get('http://inciweb.nwcg.gov/feeds/rss/incidents/')
+    .end(function (response) {
+      var xml = response.body
+      parseString(xml, function (err, result) {
+        fireArr = result.rss.channel[0].item
+        var arr = []
+        fireArr.forEach(function(el){
+          if (/[0-9]/.test(el['geo:lat'][0]) && /[0-9]/.test(el['geo:long'][0])) {
+            arr.push(el)
+          }
+        })
+        res.json(arr)
+      });
+    })
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ public/javascripts/client.js
@@ -0,0 +1,51 @@
+var Orbit = function () {}
+
+Orbit.prototype.get = function (path, fn) {
+  var req = new XMLHttpRequest()
+  req.open('GET', path)
+  req.send()
+  req.addEventListener('load', fn.bind(req))
+}
+
+var fireLocations = new Orbit()
+
+google.maps.event.addDomListener(window, 'load', initialize)
+
+function initialize () {
+  var myLatlng = new google.maps.LatLng(40.042119, -100.260929)
+  var mapOptions = {
+    zoom: 5,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROAD
+  }
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions)
+
+  // Create our info window content
+  var infoWindowContent = '<div class="info_content">' +
+    '<h3>Title</h3>' +
+    '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +
+    '</div>'
+
+  var infowindow = new google.maps.InfoWindow({
+    content: infoWindowContent
+  })
+
+  fireLocations.get('/locations', function () {
+    console.log(map)
+    var locations = JSON.parse(this.response)
+    var marker
+    locations.forEach(function (el) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(el['geo:lat'][0], el['geo:long'][0]),
+        map: map
+      })
+    })
+  })
+
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.open(map, marker)
+  })
+}
+
+google.maps.event.addDomListener(window, 'load', initialize)
