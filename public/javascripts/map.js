6137449232a2c998c758490021257a26da6a1533
Shannon Forsman <sforsman1@gmail.com>
2016-07-04 15:42:32 -0600

    update

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -89,17 +89,19 @@ fireLocations.get('/locations', function () {
 
           var tweetObj = JSON.parse(this.response)
           var tweets = tweetObj.statuses
+          var boxhtml
+
           if (tweets.length === 0) {
 
-            boxText1.innerHTML = '<div class="arrow"></div>' + signInButton + '<h2>' + elem.title + '</h2><p class="description" id="description"><strong>Description: </strong>' + elem.description[0].substring(0, 240) + '...<a href=' + elem.link[0] + ' target="_blank"> Read More</a></p><h3>Recent Tweets</h3><div class="box"><h4>Sorry, there are no tweets on this fire</h4></div>'
+            boxhtml = '<div class="arrow"></div>' + signInButton + '<h2>' + elem.title + '</h2><p class="description" id="description"><strong>Description: </strong>' + elem.description[0].substring(0, 240) + '...<a href=' + elem.link[0] + ' target="_blank"> Read More</a></p><h3>Recent Tweets</h3><div class="box"><h4>Sorry, there are no tweets on this fire</h4></div>'
 
             infowindow.setContent(boxText1)
           } else {
             tweets.forEach(function (el) {
               content += '<h4>' + el.text + '</h4>'
             })
-            boxText1.innerHTML ='<div class="arrow"></div>' + signInButton + '<h2>' + elem.title + '</h2><p class="description" id="description"><strong>Description: </strong>' + elem.description[0].substring(0, 240) + '...<a href=' + elem.link[0] + ' target="_blank"> Read More</a></p><h3>Recent Tweets</h3><div class="box">' + content + '</div>'
-            infowindow.setContent(boxText1)
+            boxhtml ='<div class="arrow"></div>' + signInButton + '<h2>' + elem.title + '</h2><p class="description" id="description"><strong>Description: </strong>' + elem.description[0].substring(0, 240) + '...<a href=' + elem.link[0] + ' target="_blank"> Read More</a></p><h3>Recent Tweets</h3><div class="box">' + content + '</div>'
+            infowindow.setContent(boxhtml)
           }
         })
       })
